4830935b186e987c813177685fa7c8a7804d6a29
DanielGonzalezCasilimas <danigonzalezcasilima@gmail.com>
2021-09-20 17:42:08 -0500

    commit inicial

---
+++ CORTE_1/ACTIVIDAD_5/EJERCICIO_4/Ejercicio_4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/*  *GROUP MEMBERS
+        Roa Ruiz Maria Camila
+        Barbosa Fierro Luis Esteban
+        Cardona Acuña David Esteban
+        González Casilimas Daniel Stevan
+     *SHIFT
+        diurnal
+     *GROUP
+        63102C / 239-2A 
+    */
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        int[,] Matriz = new int[5, 5];
+        int LONGITUD_FILA_MATRIZ = 5;
+        int LONGITUD_COLUMNA_MATRIZ = 5;
+
+        public void CargarMatriz()
+        {
+            for (int f = 0; f < LONGITUD_FILA_MATRIZ; f++){
+                for (int c = 0; c < LONGITUD_COLUMNA_MATRIZ; c++){
+                    Console.Write($"Ingrese el elemento {f+1}, {c+1} del la matriz: ");
+                    Matriz[f,c] = int.Parse(System.Console.ReadLine());
+                }
+            }
+        }
+
+        public void ImprimirMatriz()
+        {
+            Console.WriteLine("La matriz resultante es");
+
+            for (int f = 0; f < LONGITUD_FILA_MATRIZ; f++){
+                for (int c = 0; c < LONGITUD_COLUMNA_MATRIZ; c++){
+                    Console.Write("    [" + Matriz[f, c] + "] ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ImprimirMatrizInversa()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("La matriz inversa es");
+
+            for (int f = 0; f < LONGITUD_COLUMNA_MATRIZ; f++){
+                for (int c = 0; c < LONGITUD_FILA_MATRIZ; c++){
+                    Console.Write("    [" + Matriz[c, f] + "] ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program Matriz = new Program();
+            Matriz.CargarMatriz();
+            Matriz.ImprimirMatriz();
+            Matriz.ImprimirMatrizInversa();
+        }
+    }
+}
---
+++ CORTE_1/ACTIVIDAD_5/EJERCICIO_9/Ejercicio_9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*  *GROUP MEMBERS
+        Roa Ruiz Maria Camila
+        Barbosa Fierro Luis Esteban
+        Cardona Acuña David Esteban
+        González Casilimas Daniel Stevan
+     *SHIFT
+        diurnal
+     *GROUP
+        63102C / 239-2A 
+    */
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] Marco = new string [5];
+
+            Marco[0] = "111111111111111";
+            Marco[1] = "100000000000001";
+            Marco[2] = "100000000000001";
+            Marco[3] = "100000000000001";
+            Marco[4] = "111111111111111";
+
+            Console.WriteLine(" ===============");
+            Console.WriteLine("      MARCO     ");
+            Console.WriteLine(" ===============");
+            Console.WriteLine("");
+            Console.WriteLine($" {Marco[0]}");
+            Console.WriteLine($" {Marco[1]}");
+            Console.WriteLine($" {Marco[2]}");
+            Console.WriteLine($" {Marco[3]}");
+            Console.WriteLine($" {Marco[4]}");
+        }   
+    }
+}
